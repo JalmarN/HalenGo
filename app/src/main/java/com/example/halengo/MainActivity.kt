4f4b1e0144c8398ee04a2b27edd767a19b8ac714
Luddelewis <ludwig.lewis@elev.olofstrom.se>
2022-03-02 22:34:41 +0100

    fixade tvätthall knappen öppnnar activity

--- app/src/main/java/com/example/halengo/MainActivity.kt
+++ app/src/main/java/com/example/halengo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.halengo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.halengo.databinding.ActivityMainBinding
@@ -12,5 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        //kod för knappar
+        binding.tvatthallaraButton.setOnClickListener {
+            val intent = Intent(this,exempel_vindskydd::class.java)
+            startActivity(intent)
+        }
     }
 }
