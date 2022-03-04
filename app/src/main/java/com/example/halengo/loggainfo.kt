65b6da3f74f1bb24cd67fd427356eeae42cd7f4a
Luddelewis <ludwig.lewis@elev.olofstrom.se>
2022-03-04 16:38:17 +0100

    Lade till kommentar som visar var kod för att skicka in ska vara

--- app/src/main/java/com/example/halengo/loggainfo.kt
+++ app/src/main/java/com/example/halengo/loggainfo.kt
@@ -2,15 +2,18 @@ package com.example.halengo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
 import android.widget.TimePicker
 import com.example.halengo.databinding.ActivityLoggainfoBinding
+import java.util.*
+import androidx.core.view.get
 
 class loggainfo : AppCompatActivity() {
 
     lateinit var binding: ActivityLoggainfoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityLoggainfoBinding.inflate(layoutInflater)
         setContentView(binding.root)
         //sätter tidsväljaren i 24h format
@@ -21,5 +24,9 @@ class loggainfo : AppCompatActivity() {
         //sätter den maximala bokningstiden till 2 dagar i framtiden
         binding.datePicker.setMaxDate(System.currentTimeMillis() +86400000)
 
+
+        binding.skickaInformation.setOnClickListener{
+            //Här skulle kod för att skicka in vald information finnas
+        }
     }
 }
